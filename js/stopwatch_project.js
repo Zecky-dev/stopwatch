019a616a794bdbdca2f1f4a2e1f1b7e3b5584c27
Zekican Dönmez <77771717+Zecky-dev@users.noreply.github.com>
2021-03-09 00:37:45 +0300

    The files added.

---
+++ js/stopwatch_project.js
@@ -0,0 +1,72 @@
+/* Javascript Doc */
+
+// Zaman değişkenleri alınacak
+// Zaman değişkenleri = saniye , dakika , saat
+// Zaman değişkenlerinin değerleri 0 olarak verilecek
+// Saniye her 1 saniyede 1 artacak 60 saniyeye ulaştığı zaman saniye değişkeni 0'a eşitlenip dakika 1 arttırılacak dakika 60 a ulaştığında da saat 1 artacak..
+
+// Javascript values
+
+let second,minute,hour;
+second = 0;
+minute = 0;
+hour = 0;
+
+// HTML ELEMENTS
+var secondDiv,minuteDiv,hourDiv,startButton,resetButton,stopButton;
+startButton = document.getElementById('start');
+resetButton = document.getElementById('reset');
+stopButton = document.getElementById('stop');
+var clickCounter = 0;
+startButton.onclick = function(){
+    if(clickCounter<1){
+    clickCounter++;
+        myInterval = setInterval(function(){
+        second+=1;
+        secondDiv = document.querySelector('#second');
+        minuteDiv = document.querySelector('#minute');
+        hourDiv = document.querySelector('#hour');
+        if(second<10){
+        secondDiv.innerHTML = "0" + second;
+        }
+        else{
+            secondDiv.innerHTML = second;
+        }
+        if(second==60){
+            second = 0;
+            secondDiv.innerHTML = "0" + second;
+            minute+=1;
+            if(minute<10){
+                minuteDiv.innerHTML = "0" + minute;
+            }
+            else{
+                minuteDiv.innerHTML = minute;
+            }
+        }
+        if(minute==60){
+            minute = 0;
+            minuteDiv.innerHTML = "0" + minute;
+            hour+=1;
+            if(hour<10){
+                hourDiv.innerHTML = "0" + hour;
+            }
+            else{
+                hourDiv.innerHTML = hour;
+            }
+        }
+    },1000)};
+}
+resetButton.onclick = function(){
+    clickCounter = 0;
+    clearInterval(myInterval);
+    second = 0;
+    minute = 0;
+    hour = 0;
+    secondDiv.innerHTML = "0" + second;
+    minuteDiv.innerHTML = "0" + minute;
+    hourDiv.innerHTML = "0" + hour;
+}
+stopButton.onclick = function(){
+    clickCounter = 0;
+    clearInterval(myInterval);
+}
